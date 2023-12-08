c5763c043b89d7c23ea3b45f650990f073f8a2c7
Edi0501 <edytanowakowska05@gmail.com>
2023-12-08 13:24:27 +0100

    Add project files.

---
+++ dane osobowe/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace DaneOsobowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Uzupełnij formularz");
+            Console.WriteLine("Imię");
+            {
+                String imię = "imię";
+                Console.ReadLine();
+
+            }
+            Console.WriteLine("Nazwisko");
+            {
+                string nazwisko = "nazwisko";
+                Console.ReadLine();
+                 
+            }
+            Console.WriteLine("Wiek");
+            {
+                string wiek = Console.ReadLine();
+
+            }
+            
+            Console.WriteLine("Płeć, wybierz: k- kobieta, m-mężczyzna");
+            //Console.WriteLine("Wybierz 'k' - gdy jestes kobietą");
+            //Console.WriteLine("Wybierz 'm' - gdy jesteś mężczyzną");
+            //Console.WriteLine("Wybrałeś k-jesteś kobietą, wybrałeś m-jesteś mężczyzną");
+
+            char płeć1 = 'k';
+            char płeć2 = 'm';
+            Console.WriteLine("płeć1, płeć2");
+            Console.ReadLine();
+
+            Console.WriteLine("numerPESEL");
+            {
+                string numerPesel = "88022305987";
+                Console.WriteLine(numerPesel);
+            }
+            
+            
+            Console.WriteLine("Numer pracownika");
+            {
+                string numerPracownika = "56597452";
+
+                Console.WriteLine(numerPracownika);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ dane osobowe/help.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dane_osobowe
+{
+    internal class help
+      
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj płeć(k-kobieta,m-mężczyzna):");
+            string odpowiedz = Console.ReadLine();
+
+            if (odpowiedz == "k" || odpowiedz == "m")
+            {
+                Console.WriteLine("poprawna odpowiedz:+odpowiedz");
+            }
+            else
+            {
+                Console.WriteLine("Błąd Niepoprawny wybór płci.");
+            }
+        }
+
